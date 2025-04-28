e9f0b06304b459b651caa26a61422a691471df3c
teleidoskopbild <teleidoskopbild@gmail.com>
2025-04-28 19:09:36 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-200 min-h-screen">
+      <h1 className="text-3xl text-center p-4">KARDIKON</h1>
+    </div>
+  );
+}
+
+export default App;
