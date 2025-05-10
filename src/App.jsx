c6abda11e99c24ba1f5b8f92ac12c27dbc3fb9d9
teleidoskopbild <teleidoskopbild@gmail.com>
2025-05-10 19:44:23 +0200

    add StatsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AchievementsPage from "./pages/AchievementsPage";
+import StatsPage from "./pages/StatsPage";
 import GamePage from "./pages/GamePage";
 import MenuPage from "./pages/MenuPage";
 import HowToPlayPage from "./pages/HowToPlayPage";
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MenuPage />}></Route>
         <Route path="/game" element={<GamePage />}></Route>
-        <Route path="/achievements" element={<AchievementsPage />}></Route>
+        <Route path="/stats" element={<StatsPage />}></Route>
         <Route path="/how-to-play" element={<HowToPlayPage />}></Route>
       </Routes>
     </Router>
